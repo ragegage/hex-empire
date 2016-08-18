0bcd1583c905fc63707d1c34d113ad74485c6bfa
Rage Gage <ragegage@Rages-MacBook-Air.local>
2016-08-18 12:00:39 -0700

    basic styling set

--- js.js
+++ js.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("success")
+  setupBoard()
 })
+
+const setupBoard = () => {
+  $parent = $(".board")
+  for(let i = 0; i < 6; i++){
+    let $ul = $("<ul></ul>")
+    $ul.addClass("row")
+    $parent.append($ul)
+    for(let j = 0; j < 12; j++){
+      let $li = $("<li></li>")
+      $li.addClass("hex") 
+      $ul.append($li)
+    }
+  }
+}
