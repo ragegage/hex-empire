7fe5d2ef3d57ac81eb94fb0c7f83d84babdbbe1b
Rage Gage <ragegage@Rages-MacBook-Air.local>
2016-08-18 18:02:36 -0700

    adds basic classes

--- js.js
+++ js.js
@@ -1,17 +1,48 @@
 document.addEventListener("DOMContentLoaded", () => {
-  setupBoard()
-})
-
-const setupBoard = () => {
-  $parent = $(".board")
-  for(let i = 0; i < 6; i++){
-    let $ul = $("<ul></ul>")
-    $ul.addClass("row")
-    $parent.append($ul)
-    for(let j = 0; j < 12; j++){
-      let $li = $("<li></li>")
-      $li.addClass("hex") 
-      $ul.append($li)
+  const setupBoard = () => {
+    $parent = $(".board")
+    for(let i = 0; i < 6; i++){
+      let $ul = $("<ul></ul>")
+      $ul.addClass("row")
+      $parent.append($ul)
+      for(let j = 0; j < 12; j++){
+        let $li = $("<li></li>")
+        $li.addClass("hex")
+        $li.data("pos", [i, j])
+        $ul.append($li)
+      }
     }
   }
-}
+
+  const addClickHandlers = () => {
+    $(".board").on("click", ".hex", (e) => {
+      alert($(e.target).data("pos"))
+    })
+  }
+
+  class Game {
+    constructor(){
+      this.board = new Board()
+      this.players = [new Player(), new Player()]
+    }
+  }
+
+  class Army {
+    constructor(color){
+      this.color = color
+      this.strength = 0
+      this.hasMoved = false
+    }
+  }
+
+  class Tile {
+    constructor (pos) {
+      this.pos = pos
+      this.army = undefined
+      this.city = undefined
+    }
+  }
+  
+  setupBoard()
+  addClickHandlers()
+})
