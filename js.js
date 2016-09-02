9b934c446c59e916fcd9db5e5cd8d0f89a069b06
Rage Gage <ragegage@Rages-MacBook-Air.local>
2016-09-02 16:43:42 -0700

    adds a lot of features

--- js.js
+++ js.js
@@ -19,7 +19,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const addClickHandlers = (game) => {
     $(".board").on("click", ".hex", (e) => {
       // alert(`${game.currentPlayer()} moving to ${$(e.target).data('pos')}`)
-      game.move($(e.target).data('pos'))
+      game.move($(e.currentTarget).data('pos'))
       render(game)
     })
   }
@@ -34,13 +34,30 @@ document.addEventListener("DOMContentLoaded", () => {
         displayTile.removeClass()
         displayTile.addClass('hex')
         displayTile.addClass(tile.color)
-        if (tile.city)
-          displayTile.append('<city/>')
-        if (tile.army)
-          displayTile.append('<army/>')
+        if (tile.army) {
+          let $army = $('<army/>')
+          $army.addClass(tile.army.color)
+          $army.append(`<span>${tile.army.strength}</span>`)
+          // change size of circle? or not?
+          // $army.css('border-width', 15 + tile.army.strength)
+          // $army.css('border-radius', 15 + tile.army.strength)
+          displayTile.append($army)
+        }
+        if (tile.city) {
+          let $city = $('<city/>')
+          $city.addClass(tile.city.color)
+          displayTile.append($city)
+        }
+        if (tile.selected) {
+          displayTile.addClass('selected')
+        }
       })
     })
     $('.app').css('backgroundColor', game.currentPlayer().color)
+    if(game.won()){
+      console.log('won')
+      $('.app').css('backgroundColor', 'black')
+    }
   }
 
   class Game {
@@ -55,9 +72,15 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     endTurn() {
+      this.currentPlayer().hasMoved = false
       this.currentIdx++
       if(this.currentIdx >= this.players.length)
         this.currentIdx = 0
+      // sees if the current player can make a move,
+        // if not, next player's turn
+      if (this.players.every(player => player.base) && 
+        this.players[this.currentIdx].armies.every(army => army.hasMoved))
+        this.endTurn()
     }
 
     size() {
@@ -67,7 +90,13 @@ document.addEventListener("DOMContentLoaded", () => {
     move(pos) {
       let tile = this.board.get(pos)
       this.currentPlayer().move(tile)
-      this.endTurn()
+      if(this.currentPlayer().hasMoved)
+        this.endTurn()
+    }
+
+    won() {
+      if(this.players.every(player => player.base))
+        return this.players.some(player => player.base.color !== player.color)
     }
 
     render() {
@@ -79,6 +108,7 @@ document.addEventListener("DOMContentLoaded", () => {
     constructor(rows, cols){
       this.size = [rows, cols]
       this.tiles = this.addTiles(...this.size)
+      this.addCities(...this.size)
     }
 
     addTiles(rows, cols) {
@@ -90,6 +120,20 @@ document.addEventListener("DOMContentLoaded", () => {
         arr.push(row)
       }
       return arr
+    }
+
+    addCities(rows, cols) {
+      let maxNumCities = 4
+      const odds = maxNumCities / (rows * cols)
+
+      // loop through tiles
+        // odds are maxNumCities / numTiles
+        // odds on put a city on a given tile
+      for(let i = 0; i < rows; i++){
+        for(let j = 0; j < cols; j++)
+          if(Math.random() < odds)
+            this.tiles[i][j].city = new City("black", this.tiles[i][j])
+      }
     }
 
     valid ([row, col]) {
@@ -109,10 +153,12 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   class Army {
-    constructor(color){
+    constructor(color, tile, player){
       this.color = color
-      this.strength = 0
+      this.strength = 1
       this.hasMoved = false
+      this.tile = tile
+      this.player = player
     }
 
     move() {
@@ -126,6 +172,50 @@ document.addEventListener("DOMContentLoaded", () => {
     increaseStrength(amount) {
       this.strength += amount
     }
+
+    moveTo(newTile) {
+      if(this.tile.fullNeighbors().includes(newTile)) {
+        this.tile.army = undefined
+        this.tile = newTile
+        if(this.tile.army){ // runs into another army
+          if(this.tile.army.color === this.color) { // same team
+            this.tile.army.strength += this.strength
+            this.die()
+          } else {
+            this.increaseStrength(1) // battle logic
+            if(this.tile.army.strength > this.strength){ // losing battle
+              this.tile.army.strength -= this.strength
+              this.die()
+            } else { // winning battle
+              this.strength -= this.tile.army.strength
+              this.tile.army.die()
+              this.tile.army = this
+              this.tile.color = this.color
+              this.tile.neighbors().forEach(neighbor => neighbor.color = this.color)
+              if(this.tile.city) {
+                this.tile.city.color = this.color
+                this.player.cities.push(this.tile.city)
+              }
+            }
+          }
+        } else {
+          this.tile.army = this
+          this.tile.color = this.color
+          if(this.tile.city) {
+            this.tile.city.color = this.color
+            this.player.cities.push(this.tile.city)
+          }
+          this.tile.neighbors().forEach(neighbor => neighbor.color = this.color)
+        }
+      } else {
+        throw "invalid move"
+      }
+    }
+
+    die() {
+      this.tile = undefined
+      this.player.armies = this.player.armies.filter(army => army !== this)
+    }
   }
 
   class Tile {
@@ -135,9 +225,10 @@ document.addEventListener("DOMContentLoaded", () => {
       this.army = undefined
       this.city = undefined
       this.color = undefined
+      this.selected = false
     }
 
-    neighbors () {
+    fullNeighbors () {
       const x = this.pos[0]
       const y = this.pos[1]
       let neighbors = [
@@ -152,7 +243,31 @@ document.addEventListener("DOMContentLoaded", () => {
         neighbors[3] = [x-1, y-1]
         neighbors[5] = [x-1, y+1]
       }
-      return neighbors.map(pos => this.board.get(pos)).filter(neighbor => neighbor)
+      return neighbors.map(pos => this.board.get(pos))
+               .filter(neighbor => neighbor)
+    }
+
+    neighbors () {
+      return this.fullNeighbors()
+               .filter(neighbor => !neighbor.army || !neighbor.city)
+    }
+  }
+
+  class City {
+    constructor(color, tile, player) {
+      this.color = color
+      this.tile = tile
+      this.player = player
+    }
+
+    naturalGrowth() {
+      // if city has an army, army grows by 15
+      if (this.tile.army)
+        this.tile.army.increaseStrength(15)
+      else {
+        this.tile.army = new Army(this.color, this.tile, this.player)
+        this.player.armies.push(this.tile.army)
+      }
     }
   }
   
@@ -161,23 +276,49 @@ document.addEventListener("DOMContentLoaded", () => {
       this.color = color
       this.base = undefined
       this.armies = []
+      this.hasMoved = false
+      this.selectedArmy = undefined
+      this.cities = []
     }
 
     setHomeBase(tile) {
       this.base = tile
       tile.color = this.color
-      tile.city = true
-      tile.army = new Army(this.color)
+      tile.city = new City(this.color, tile, this)
+      tile.army = new Army(this.color, tile, this)
       this.armies.push(tile.army)
+      this.cities.push(tile.city)
     }
 
     move(tile) {
       if (this.base === undefined){
         this.setHomeBase(tile)
         tile.neighbors().forEach(tile => tile.color = this.color)
+        this.hasMoved = true
       } else {
-        // if (this.armies.includes(tile.army))
-        tile.neighbors().forEach(tile => tile.color = this.color)
+        if (this.armies.every(army => army.hasMoved)) {
+          this.cities.forEach(city => city.naturalGrowth())
+          this.hasMoved = true
+          this.selectedArmy = undefined
+        } else if (this.selectedArmy) {
+          // allow user to move army from current location to new location... 
+            // if it's a valid move
+          try {
+            const oldTile = this.selectedArmy.tile
+            this.selectedArmy.moveTo(tile)
+            this.cities.forEach(city => city.naturalGrowth())
+            this.hasMoved = true
+            oldTile.selected = false
+            this.selectedArmy = undefined
+          } catch(err) {
+            this.hasMoved = false
+          }
+        } else if (this.armies.includes(tile.army)){
+          // if the tile clicked on has an army and it is one of our armies,
+          tile.selected = true
+          this.selectedArmy = tile.army
+          // tile.neighbors().forEach(tile => tile.color = "white")
+        }
       }
     }
 
@@ -193,4 +334,5 @@ document.addEventListener("DOMContentLoaded", () => {
   let game = new Game(5, 11)
   setupBoard(game)
   addClickHandlers(game)
+  render(game)
 })
